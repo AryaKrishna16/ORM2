7113ce2a4fb2235d90615ddfc1b4913e07f1d2ee
AryaKrishna16 <aryakrishna982@gmail.com>
2025-09-16 15:53:18 +0530

    hi

---
+++ car/carapp/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib import admin
+class Car(models.Model):
+    car_name = models.CharField()
+    car_colour = models.CharField()
+    release_date = models.DateField()
+    car_model = models.CharField()
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('car_name','car_colour','release_date','car_model')
+
